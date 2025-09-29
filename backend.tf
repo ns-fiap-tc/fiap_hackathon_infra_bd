e392199561e9ad64dc69684aacf32307f6008f5d
Nicolas Silva <nicolasoliveirasilva98@gmail.com>
2025-09-29 02:42:03 -0300

    Update Terraform backend configuration and add deployment workflows

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-#terraform {
-# backend "s3" {
-#   bucket         = "lanchonete-tfstate-bucket"
-#   key            = "infra-bd/terraform.tfstate"
-#   region         = "us-east-1"
-#   dynamodb_table = "terraform-lock"
-#   encrypt        = true
-# }
-#}
+terraform {
+ backend "s3" {
+   bucket         = "hacka-tfstate-bucket"
+   key            = "infra-bd/terraform.tfstate"
+   region         = "us-east-1"
+   dynamodb_table = "terraform-lock"
+   encrypt        = true
+ }
+}
